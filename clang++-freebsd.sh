2e4c7f69a57cd51f8db4b17a645a951dfff0bcf5
S Harris <>
2021-10-22 11:17:44 +0000

    Add example config and shim scripts for cross compiling Aarch64 FreeBSD compiler and libraries

---
+++ clang++-freebsd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Quick and dirty wrapper to call the right clang with the right target and system library path.
+# I'm not sure why the build system doesn't do this itself.
+# See config.toml.sarah for more information.
+
+/home/cheribuild/output/morello-sdk/bin/clang++ --sysroot /home/cheribuild/output/freebsd-aarch64/ -target aarch64-unknown-freebsd "$@"
