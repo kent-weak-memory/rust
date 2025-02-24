dcadf0588d0517fceca0f4c0b18665de0e4e22a2
S Harris <>
2025-02-24 12:46:38 +0000

    Work in progress: fix pattern matching in test causing a false positive on Morello

--- tests/ui/consts/const-eval/ub-uninhabit.rs
+++ tests/ui/consts/const-eval/ub-uninhabit.rs
@@ -1,6 +1,6 @@
 // Strip out raw byte dumps to make comparison platform-independent:
 // normalize-stderr-test "(the raw bytes of the constant) \(size: [0-9]*, align: [0-9]*\)" -> "$1 (size: $$SIZE, align: $$ALIGN)"
-// normalize-stderr-test "([0-9a-f][0-9a-f] |╾─*a(lloc)?[0-9]+(\+[a-z0-9]+)?─*╼ )+ *│.*" -> "HEX_DUMP"
+// normalize-stderr-test "([0-9a-f][0-9a-f] |╾─*a(lloc)?[0-9]+(\+[a-z0-9]+)?─*╼ )+(__ )* *│.*" -> "HEX_DUMP"
 
 use std::mem;
 
@@ -16,7 +16,7 @@ union MaybeUninit<T: Copy> {
 const BAD_BAD_BAD: Bar = unsafe { MaybeUninit { uninit: () }.init };
 //~^ ERROR evaluation of constant value failed
 
-const BAD_BAD_REF: &Bar = unsafe { mem::transmute(1usize) };
+const BAD_BAD_REF: &Bar = unsafe { mem::transmute(1usize as *const ()) };
 //~^ ERROR it is undefined behavior to use this value
 
 const BAD_BAD_ARRAY: [Bar; 1] = unsafe { MaybeUninit { uninit: () }.init };
