1d53bc5b5f6f19c7965efddcc7994a770a939f4d
S Harris <>
2025-04-14 12:12:23 +0100

    Work in progress: fix layout of value used to create slice

--- tests/ui/consts/const-eval/dangling.rs
+++ tests/ui/consts/const-eval/dangling.rs
@@ -2,7 +2,7 @@ use std::mem;
 
 // Make sure we error with the right kind of error on a too large slice.
 const TEST: () = { unsafe {
-    let slice: *const [u8] = mem::transmute((1usize, usize::MAX));
+    let slice: *const [u8] = mem::transmute((1usize as *const u8, usize::MAX as *const u8));
     let _val = &*slice; //~ ERROR: evaluation of constant value failed
     //~| slice is bigger than largest supported object
 } };
