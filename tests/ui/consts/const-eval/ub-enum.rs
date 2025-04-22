738e36d27017cf5c82acebfbb61a0ae8842e5ea4
S Harris <>
2025-04-22 11:55:59 +0100

    Work in progress: disable test cases that rely on pointers having same layout as usize

--- tests/ui/consts/const-eval/ub-enum.rs
+++ tests/ui/consts/const-eval/ub-enum.rs
@@ -27,11 +27,11 @@ const GOOD_ENUM: Enum = unsafe { mem::transmute(0usize) };
 const BAD_ENUM: Enum = unsafe { mem::transmute(1usize) };
 //~^ ERROR is undefined behavior
 
-const BAD_ENUM_PTR: Enum = unsafe { mem::transmute(&1) };
-//~^ ERROR evaluation of constant value failed
+// const BAD_ENUM_PTR: Enum = unsafe { mem::transmute(&1) };
+// ERROR evaluation of constant value failed
 
-const BAD_ENUM_WRAPPED: Wrap<Enum> = unsafe { mem::transmute(&1) };
-//~^ ERROR evaluation of constant value failed
+// const BAD_ENUM_WRAPPED: Wrap<Enum> = unsafe { mem::transmute(&1) };
+// ERROR evaluation of constant value failed
 
 // # simple enum with discriminant 2
 
@@ -44,11 +44,11 @@ enum Enum2 {
 
 const BAD_ENUM2: Enum2 = unsafe { mem::transmute(0usize) };
 //~^ ERROR is undefined behavior
-const BAD_ENUM2_PTR: Enum2 = unsafe { mem::transmute(&0) };
-//~^ ERROR evaluation of constant value failed
+// const BAD_ENUM2_PTR: Enum2 = unsafe { mem::transmute(&0) };
+// ERROR evaluation of constant value failed
 // something wrapping the enum so that we test layout first, not enum
-const BAD_ENUM2_WRAPPED: Wrap<Enum2> = unsafe { mem::transmute(&0) };
-//~^ ERROR evaluation of constant value failed
+// const BAD_ENUM2_WRAPPED: Wrap<Enum2> = unsafe { mem::transmute(&0) };
+// ERROR evaluation of constant value failed
 
 // Undef enum discriminant.
 #[repr(C)]
@@ -61,8 +61,8 @@ const BAD_ENUM2_UNDEF : Enum2 = unsafe { MaybeUninit { uninit: () }.init };
 //~| uninitialized
 
 // Pointer value in an enum with a niche that is not just 0.
-const BAD_ENUM2_OPTION_PTR: Option<Enum2> = unsafe { mem::transmute(&0) };
-//~^ ERROR evaluation of constant value failed
+// const BAD_ENUM2_OPTION_PTR: Option<Enum2> = unsafe { mem::transmute(&0) };
+// ERROR evaluation of constant value failed
 
 // # valid discriminant for uninhabited variant
 
