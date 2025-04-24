024a62e6cb486b174271f3342438c6f1b9f35aa7
S Harris <>
2025-04-24 10:15:30 +0100

    Work in progress: fix test assuming pointers and usize have the same sizes in memory

--- tests/ui/consts/const-eval/ub-upvars.rs
+++ tests/ui/consts/const-eval/ub-upvars.rs
@@ -1,10 +1,11 @@
 // stderr-per-bitwidth
+// stderr-per-cheri
 #![allow(invalid_value)] // make sure we cannot allow away the errors tested here
 
 use std::mem;
 
 const BAD_UPVAR: &dyn FnOnce() = &{ //~ ERROR it is undefined behavior to use this value
-    let bad_ref: &'static u16 = unsafe { mem::transmute(0usize) };
+    let bad_ref: &'static u16 = unsafe { mem::transmute(0usize as *const u16) };
     let another_var = 13;
     move || { let _ = bad_ref; let _ = another_var; }
 };
