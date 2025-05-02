22b659700e0453d558d374abd0b0e08a99335b5a
S Harris <>
2025-05-02 10:23:55 +0100

    Work in progress: skip test which is not CHERI compatible

--- tests/ui/issues/issue-5718.rs
+++ tests/ui/issues/issue-5718.rs
@@ -1,5 +1,7 @@
 // run-pass
 // pretty-expanded FIXME #23616
+// ignore-purecap: this transmute does not work on CHERI targets, and I
+// (seharris) don't want to risk breaking the test trying to avoid it.
 
 struct Element;
 
