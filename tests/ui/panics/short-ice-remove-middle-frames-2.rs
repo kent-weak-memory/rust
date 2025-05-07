36b5d64e263e5a5232f25a5b18f0aa72a98519f6
S Harris <>
2025-05-07 13:18:30 +0100

    Work in progress: fix test by using normalisation to ignore libunwind warnings

--- tests/ui/panics/short-ice-remove-middle-frames-2.rs
+++ tests/ui/panics/short-ice-remove-middle-frames-2.rs
@@ -10,6 +10,9 @@
 // ignore-fuchsia Backtraces not symbolized
 // ignore-msvc the `__rust_{begin,end}_short_backtrace` symbols aren't reliable.
 
+// TODO(seharris): remove when and if we fix libunwind problems
+// normalize-stderr-test "libunwind: evaluated out-of-bounds/invalid CFA expression for pc.*\n\n" -> ""
+
 /// This test case make sure that we can have multiple pairs of `__rust_{begin,end}_short_backtrace`
 
 #[inline(never)]
