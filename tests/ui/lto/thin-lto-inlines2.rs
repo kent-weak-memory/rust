dd6ead2feaf90718d336375190ad25736006224b
S Harris <>
2025-05-07 11:13:45 +0100

    Work in progress: skip test that is incompatible with CHERI

--- tests/ui/lto/thin-lto-inlines2.rs
+++ tests/ui/lto/thin-lto-inlines2.rs
@@ -5,6 +5,14 @@
 // no-prefer-dynamic
 // ignore-emscripten can't inspect instructions on emscripten
 
+// ignore-aarch64-unknown-freebsd-purecap
+// CHERI BSD appears to seal function pointers, presumably as part of loading
+// and relocation.
+// It uses object type one, which means (if I understand correctly) that only
+// way to unseal the capability is to branch to it.
+// Because of this, there is no tidy way to read bytes via pointers to
+// functions, and this test cannot be used on CHERI BSD targets.
+
 // We want to assert here that ThinLTO will inline across codegen units. There's
 // not really a great way to do that in general so we sort of hack around it by
 // praying two functions go into separate codegen units and then assuming that
