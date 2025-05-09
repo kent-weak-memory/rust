27668093a7711ddc815eb8a949a5fd5e4e7dbdd1
S Harris <>
2025-05-09 09:47:38 +0100

    Work in progress: fix test by disabling cases that use usize on CHERI targets

--- tests/ui/traits/new-solver/pointer-like.rs
+++ tests/ui/traits/new-solver/pointer-like.rs
@@ -1,5 +1,6 @@
 // compile-flags: -Ztrait-solver=next
 
+#![feature(cfg_target_abi)]
 #![feature(pointer_like_trait)]
 
 use std::marker::PointerLike;
@@ -7,6 +8,7 @@ use std::marker::PointerLike;
 fn require_(_: impl PointerLike) {}
 
 fn main() {
+    #[cfg(not(target_abi = "purecap"))]
     require_(1usize);
     require_(1u16);
     //~^ ERROR `u16` needs to have the same ABI as a pointer
