d53ee2efb0b8559c3e0ab37bc6dc613728e9545c
S Harris <>
2025-07-30 11:16:46 +0100

    Work in progress: revert changes to test now underlying cause of changed optimisation output is fixed

--- tests/mir-opt/dont_yeet_assert.rs
+++ tests/mir-opt/dont_yeet_assert.rs
@@ -6,9 +6,6 @@
 // Want to make sure this assertion isn't compiled away in generic code.
 
 // EMIT_MIR dont_yeet_assert.generic.InstSimplify.diff
-// EMIT_MIR_FOR_EACH_CHERI
-// TODO(seharris): why does this produce slightly different optimised MIR when
-//                 testing Morello?
 pub fn generic<T>() {
     core::intrinsics::assert_mem_uninitialized_valid::<&T>();
 }
